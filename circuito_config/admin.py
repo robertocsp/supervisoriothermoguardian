4f812131bae8b574612c55b0d868496c2e3bcf0d
robertocsp <80.pereira@gmail.com>
2019-12-03 10:58:42 -0300

    export-to-excel

--- circuito_config/admin.py
+++ circuito_config/admin.py
@@ -1,6 +1,9 @@
+import datetime
 from django.contrib import admin
-from . import models
 
+from . import models
+from django.http import HttpResponse, HttpResponseRedirect
+from openpyxl import Workbook
 
 @admin.register(models.Cliente)
 class ClienteAdmin(admin.ModelAdmin):
@@ -35,9 +38,68 @@ class SuperaquecimentoconfigAdmin(admin.ModelAdmin):
 class SuperaquecimentologAdmin(admin.ModelAdmin):
     list_display = ('resultado', 'superaquecimento', 'superaquecimentoconfig', 'datahora')
 
+    # list_per_page = sys.maxsize
+    list_per_page = 999
+    actions = ['export_excel']
+
+    def export_excel(self, request, queryset):
+        response = HttpResponse(
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        )
+        response['Content-Disposition'] = 'attachment; filename={date}-superaquecimento.xlsx'.format(
+            date=datetime.datetime.now().strftime('%Y-%m-%d'),
+        )
+        workbook = Workbook()
+
+        # Get active worksheet/tab
+        worksheet = workbook.active
+        worksheet.title = 'Superaquecimento'
+
+        # Define the titles for columns
+        columns = [
+            'Resultado',
+            'Superaquecimento',
+            'DataHora',
+
+        ]
+        row_num = 1
+
+        # Assign the titles for each cell of the header
+        for col_num, column_title in enumerate(columns, 1):
+            cell = worksheet.cell(row=row_num, column=col_num)
+            cell.value = column_title
+
+        # Iterate through all movies
+        for item in queryset:
+            row_num += 1
+
+            # Define the data for each cell in the row
+            row = [
+                item.resultado,
+                'Não' if item.superaquecimento else 'Sim',
+                item.datahora,
+
+            ]
+
+            # Assign the data for each cell of the row
+            for col_num, cell_value in enumerate(row, 1):
+                cell = worksheet.cell(row=row_num, column=col_num)
+                cell.value = cell_value
+
+        workbook.save(response)
+
+        return response
+
+    export_excel.short_description = "Exportar para Excel itens selecionados"
+
+
+
 admin.site.register(models.Circuito)
 admin.site.register(models.Modulo)
 admin.site.register(models.Parametro)
+
+
+
 
 
 
